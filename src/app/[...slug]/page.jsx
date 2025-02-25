14dde4eea244edf9988dbeec8dc4e76f8bd8e497
rodrigo solis <rodrigo.solis.g@gmail.com>
2025-02-24 23:40:32 -0300

    ya estan funcionando la carga de documentos y el procesamiento con content layer

---
+++ src/app/[...slug]/page.jsx
@@ -0,0 +1,30 @@
+"use client";
+import { allPages } from "contentlayer/generated";
+import { notFound } from "next/navigation";
+import { useMDXComponent } from "next-contentlayer/hooks";
+
+const Page = ({ params }) => {
+  // Unir el slug en caso de que haya múltiples niveles en la URL
+  const slug = params.slug.join("/");
+
+  // Buscar la página correspondiente en Contentlayer
+  const page = allPages.find((p) => p._raw.flattenedPath === slug);
+
+  // Si la página no existe, lanzar un error 404
+  if (!page) {
+    return notFound();
+  }
+
+  // Obtener el componente MDX
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const MDXContent = useMDXComponent(page.body.code);
+
+  return (
+    <div className="MainContent">
+      {/* <h1>{page.title}</h1> */}
+      <MDXContent />
+    </div>
+  );
+};
+
+export default Page;
